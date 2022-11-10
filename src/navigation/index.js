dacddefae323e4c8258911c7e192de4744fde413
billhikmah <billhikmah1@yahoo.co.id>
2022-11-10 16:43:56 +0700

    first upload

---
+++ src/navigation/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+import Home from '../screen/Home';
+import Detail from '../screen/Detail';
+import Signin from '../screen/Signin';
+
+export default function MainStackNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/screen/Signin/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function Signin() {
+  return (
+    <View>
+      <Text>Signin Screen</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({});
