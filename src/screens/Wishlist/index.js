e4a1296702ea20bbd7f4c3ed34b6fbe1123273cd
billhikmah <billhikmah1@yahoo.co.id>
2022-11-11 00:50:22 +0700

    Feat: Upload my wishlist screen

---
+++ src/screens/Wishlist/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function Wishlist(props) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Wishlist Screen</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({});
