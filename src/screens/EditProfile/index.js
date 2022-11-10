047737597b6e1316f38bb23b5d29388b784132b8
billhikmah <billhikmah1@yahoo.co.id>
2022-11-11 00:49:41 +0700

    Feat: Upload edit profile screen

---
+++ src/screens/EditProfile/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function Edit(props) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Edit Screen</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({});
