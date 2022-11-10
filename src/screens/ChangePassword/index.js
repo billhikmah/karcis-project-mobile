d58385da586f8c80cff823997c8af3e552fed6bd
billhikmah <billhikmah1@yahoo.co.id>
2022-11-11 00:49:10 +0700

    Feat: Upload change password screen

---
+++ src/screens/ChangePassword/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function ChangePassword(props) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>ChangePassword Screen</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({});
