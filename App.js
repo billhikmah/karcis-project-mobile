4596fbb967eabc4453cc24f502747b04f91741df
billhikmah <billhikmah1@yahoo.co.id>
2022-11-29 23:46:41 +0700

    Feat: Add Redux Setup

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import MainStackNavigator from './src/navigation';
+import {Provider as StoreProvider} from 'react-redux';
+import stores from './src/store';
+const {store, persistor} = stores;
+import {PersistGate} from 'redux-persist/integration/react';
 
 export default function App() {
-  return <MainStackNavigator />;
+  return (
+    <StoreProvider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <MainStackNavigator />
+      </PersistGate>
+    </StoreProvider>
+  );
 }
