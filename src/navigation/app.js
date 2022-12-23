1ae733741933558b1a63e9015cc67190ecd25187
billhikmah <billhikmah1@yahoo.co.id>
2022-12-23 15:05:09 +0700

    Feat: Add screens to navigation

--- src/navigation/app.js
+++ src/navigation/app.js
@@ -15,15 +15,62 @@ import MyBooking from '../screens/MyBooking';
 import Order from '../screens/Order';
 import Payment from '../screens/Payment';
 import Wishlist from '../screens/Wishlist';
+import Search from '../screens/Search';
+
+import DrawerContent from '../components/DrawerContent';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+import Header from '../components/Header/default.js';
+import HeaderSubScreen from '../components/Header/subscreen.js';
 
 function MenuNavigator() {
   return (
     // DAFTARKAN MENU YANG NANTINYA AKAN MASUK KE DALAM DRAWER DISINI
-    <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={Home} />
-      <Drawer.Screen name="Profile" component={Profile} />
-      <Drawer.Screen name="My Booking" component={MyBooking} />
-      <Drawer.Screen name="My Wishlist" component={Wishlist} />
+    <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+      <Drawer.Screen
+        name="Home"
+        component={Home}
+        options={{
+          header: props => <Header {...props} />,
+          drawerIcon: ({size, color}) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          header: props => <HeaderSubScreen {...props} title="Profile" />,
+          drawerIcon: ({size, color}) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+          drawerItemStyle: {
+            display: 'none',
+          },
+        }}
+      />
+      <Drawer.Screen
+        name="My Booking"
+        component={MyBooking}
+        options={{
+          header: props => <HeaderSubScreen {...props} title="My Booking" />,
+          drawerIcon: ({size, color}) => (
+            <Icon name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="My Wishlist"
+        component={Wishlist}
+        options={{
+          header: props => <HeaderSubScreen {...props} title="My Wishlist" />,
+          drawerIcon: ({size, color}) => (
+            <Icon name="heart" color={color} size={size} />
+          ),
+          // headerShown: false,
+        }}
+      />
     </Drawer.Navigator>
   );
 }
@@ -38,11 +85,46 @@ export default function AppStackNavigator() {
         options={{headerShown: false}}
       />
       <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Detail" component={Detail} />
-      <Stack.Screen name="Order" component={Order} />
-      <Stack.Screen name="Payment" component={Payment} />
-      <Stack.Screen name="Edit" component={EditProfile} />
-      <Stack.Screen name="Change Password" component={ChangePassword} />
+      <Stack.Screen
+        name="Detail"
+        component={Detail}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Order"
+        component={Order}
+        options={{
+          header: props => <HeaderSubScreen {...props} title="Checkout" />,
+        }}
+      />
+      <Stack.Screen
+        name="Payment"
+        component={Payment}
+        options={{
+          header: props => <HeaderSubScreen {...props} title="Payment" />,
+        }}
+      />
+      <Stack.Screen
+        name="Edit"
+        component={EditProfile}
+        options={{
+          header: props => <HeaderSubScreen {...props} title="Edit Profile" />,
+        }}
+      />
+      <Stack.Screen
+        name="Change Password"
+        component={ChangePassword}
+        options={{
+          header: props => <HeaderSubScreen {...props} title="Edit Password" />,
+        }}
+      />
+      <Stack.Screen
+        name="Search"
+        component={Search}
+        options={{
+          header: props => <HeaderSubScreen {...props} title="Search" />,
+        }}
+      />
     </Stack.Navigator>
   );
 }
